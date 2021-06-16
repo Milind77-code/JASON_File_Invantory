22162aa17c4e9b3b1edca20e25fb33696b32cd3e
Milind_77 <milinddhote77@gmail.com>
2021-06-16 05:54:16 +0530

    [milind] Add: Json Inventory Management

---
+++ JsonInventory/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace JsonInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Json Inventory Management");
+            Console.WriteLine("Checking Prodects Price");
+            Detail Data = new Detail();
+            Data.GetData();
+        }
+    }
+}
